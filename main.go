0d1cdde1e7051f84f2f6d3bf885e49b9a1f9ea43
Kyle Phillips <kphillips375@yahoo.com>
2022-10-11 22:48:53 -0400

    setup muxserver, get blockchain function and dependencies properly imported

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 const difficulty = 1
-
-var GoBlock []Block
 
 type Block struct {
 	Index      int
@@ -20,31 +26,60 @@ type Block struct {
 	Nonce      string
 }
 
+var GoBlock []Block
+
+var mutex = &sync.Mutex{}
+
 func main() {
-	err := godotdenv.Load()
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// goroutine for genesis block
 	go func() {
 		t := time.Now()
 		genesisBlock := Block{}
 		genesisBlock = Block{0, t.String(), 0, calculateHash(genesisBlock), "", difficulty, ""}
+		spew.Dump(genesisBlock)
+		mutex.Lock()
+		GoBlock = append(GoBlock, genesisBlock)
+		mutex.Unlock()
 	}()
 	log.Fatal(run())
 }
 
 func run() error {
-
+	mux := makeRouter()
+	httpPort := os.Getenv("PORT")
+	log.Println("HTTP server is running and listening on port:", httpPort)
+	s := &http.Server{
+		Addr:           ":" + httpPort,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func makeRouter() http.Handler {
-	HandleFunc("/", handleGetBlockchain).Methods("GET")
-	HandleFunc("/", handleWriteBlock).Methods("POST")
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
 }
 
-func handleGetBlockchain() {
-
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(GoBlock,""," ")
+	if err != nil {
+		http.Error(w, err.Error(),http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, string(bytes))
 }
 
 func handleWriteBlock() {
